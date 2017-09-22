b3d3ca00a340f7f3286495a0bdb1730d1c3ec211
César Alejandro Morales Armijo <mugiwara@cesar.local>
2017-09-22 18:10:40 -0300

    adding xls for test

--- connections.js
+++ connections.js
@@ -18,7 +18,7 @@ module.exports.connections = {
       host: '0.0.0.0',
       user: 'root',
       port:'3306',
-      password: '12345',
+      password: '123456',
       database: 'aog_development'
     }
   };
--- globals.js
+++ globals.js
@@ -2,5 +2,5 @@
 var path = require('path');
 
 module.exports.globals = {
-  EXCELPATH : path.resolve('xls_csv', 'AOG_WP.xls')
+  EXCELPATH : path.resolve('xls_csv', 'AOG_WP_22092017.xls')
 };
